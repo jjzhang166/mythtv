jjzhang166/mythtv

commit 9b22460f53bf6313516f33987e4260897c6076a3

Merge of various recorder changes from mythtv-rec2.
 * Fixes #9191. Channel switching doesn't work with cx18 driver.
 * Fixes #9451. IPTVRecorder doesn't produce proper TS packets for big DVB tables.
 * Fixes #9709. TFW IOBOUND.
 * This also fixes pause status locking in various recorders.
 * This also replaces a few spinlocks with QWaitConditions.
 * This also adds a DVB ASI recorder implementation in just 1033 lines. (Uninteresting to most).
 * This makes changes to the DeviceReadBuffer to make it more efficient and quicker to respond.
 * This changes the ThreadedFileWriter so that it can expand the level of buffering it does when there are temporary slowdowns in writing to disk.

This will probably cause some regressions, that's why I'm pushing this on a Monday. This refactors all the recorders and stream readers to eliminate a lot of duplicate code, and also fixes a number of small bugs and long time annoyances see the dkristjansson/mythtv-rec branch for details.

mythtv/programs/mythbackend/scheduler.h
@@ -147,6 +147,26 @@ class Scheduler : public QThread
                          const RecList &reclist);
     void FillDirectoryInfoCache(bool force = false);
 
+    int CalcTimeToNextHandleRecordingEvent(
+        const QDateTime &curtime,
+        RecConstIter startIter, const RecList &reclist,
+        int prerollseconds, int max_sleep /*ms*/);
+    void OldRecordedFixups(void);
+    bool HandleReschedule(void);
+    bool HandleRunSchedulerStartup(
+        int prerollseconds, int idleWaitForRecordingTime);
+    void HandleWakeSlave(RecordingInfo &ri, int prerollseconds);
+    bool HandleRecording(
+        RecordingInfo &ri, bool &statuschanged, int prerollseconds);
+    void HandleTuning(
+        RecordingInfo &ri, bool &statuschanged);
+    void HandleRecordingStatusChange(
+        RecordingInfo &ri, RecStatusTypes recStatus, const QString &details);
+    void HandleIdleShutdown(
+        bool &blockShutdown, QDateTime &idleSince, int prerollseconds,
+        int idleTimeoutSecs, int idleWaitForRecordingTime);
+
+
     MythDeque<int> reschedQueue;
     QMutex schedLock;
     QMutex recordmatchLock;

commit 61724134a1b63f4087fdd6e94470aed57fd6f43a

Fixes REC_PENDING and wakeup handling handling.
I accidentally broke this with commit [9b22460f53bf6313516f33987e4260897c6076a3] in master.

This also fixes a pre-existing memory leak in the REC_PENDING handling.

mythtv/programs/mythbackend/scheduler.cpp
@@ -1812,6 +1812,8 @@ void Scheduler::run(void)
             {
                 int sched_sleep = (secs_to_next - schedRunTime - 1) * 1000;
                 sched_sleep = min(sched_sleep, maxSleep);
+                if (secs_to_next < prerollseconds + (maxSleep/1000))
+                    sched_sleep = min(sched_sleep, 5000);
                 LOG(VB_SCHEDULE, LOG_INFO,
                     QString("sleeping for %1 ms (interuptable)")
                         .arg(sched_sleep));
@@ -1892,7 +1894,7 @@ void Scheduler::run(void)
 
         /// Wake any slave backends that need waking
         curtime = QDateTime::currentDateTime();
-        for (RecIter it = startIter; it != reclist.end() && !done; ++it)
+        for (RecIter it = startIter; it != reclist.end(); ++it)
         {
             int secsleft = curtime.secsTo((*it)->GetRecordingStartTime());
             if ((secsleft - prerollseconds) <= wakeThreshold)
@@ -2146,8 +2148,6 @@ bool Scheduler::HandleRunSchedulerStartup(
 void Scheduler::HandleWakeSlave(RecordingInfo &ri, int prerollseconds)
 {
     static const int sysEventSecs[5] = { 120, 90, 60, 30, 0 };
-    QString sysEventKey;
-    QList<QString> sysEvents[4];
 
     QDateTime curtime = QDateTime::currentDateTime();
     QDateTime nextrectime = ri.GetRecordingStartTime();
@@ -2157,8 +2157,7 @@ void Scheduler::HandleWakeSlave(RecordingInfo &ri, int prerollseconds)
     if (tvit == m_tvList->end())
         return;
 
-    sysEventKey = QString("%1:%2").arg(ri.GetChanID())
-        .arg(nextrectime.toString(Qt::ISODate));
+    QString sysEventKey = ri.MakeUniqueKey();
 
     int i = 0;
     bool pendingEventSent = false;
@@ -2173,10 +2172,35 @@ void Scheduler::HandleWakeSlave(RecordingInfo &ri, int prerollseconds)
                     QString("REC_PENDING SECS %1").arg(secsleft), &ri);
             }
 
-            sysEvents[i].append(sysEventKey);
+            sysEvents[i].insert(sysEventKey);
             pendingEventSent = true;
         }
         i++;
+    }
+
+    // cleanup old sysEvents once in a while
+    QSet<QString> keys;
+    for (i = 0; sysEventSecs[i] != 0; i++)
+    {
+        if (sysEvents[i].size() < 20)
+            continue;
+
+        if (keys.empty())
+        {
+            RecConstIter it = reclist.begin();
+            for ( ; it != reclist.end(); ++it)
+                keys.insert((*it)->MakeUniqueKey());
+            keys.insert("something");
+        }
+
+        QSet<QString>::iterator sit = sysEvents[i].begin();
+        while (sit != sysEvents[i].end())
+        {
+            if (!keys.contains(*sit))
+                sit = sysEvents[i].erase(sit);
+            else
+                ++sit;
+        }
     }
 
     EncoderLink *nexttv = *tvit;

mythtv/programs/mythbackend/scheduler.h
@@ -13,6 +13,7 @@ using namespace std;
 #include <QThread>
 #include <QMutex>
 #include <QMap>
+#include <QSet>
 
 // MythTV headers
 #include "recordinginfo.h"
@@ -206,6 +207,8 @@ class Scheduler : public QThread
     QMap<QString, FileSystemInfo> fsInfoCache;
 
     int error;
+
+    QSet<QString> sysEvents[4];
 
     // Try to avoid LiveTV sessions until this time
     QDateTime livetvTime;